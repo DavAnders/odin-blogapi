89522ee90efc2b67291aa4b98a117c1eceb9fd30
DavAnders <123442746+DavAnders@users.noreply.github.com>
2024-05-15 13:27:51 -0500

    Add vercel config

--- frontend/src/registerForm.jsx
+++ frontend/src/registerForm.jsx
@@ -1,5 +1,6 @@
 import { useState, useContext } from "react";
 import { AuthContext } from "./auth/AuthContext";
+import DOMPurify from "dompurify";
 
 function RegistrationForm() {
   const [username, setUsername] = useState("");
@@ -7,8 +8,17 @@ function RegistrationForm() {
   const [email, setEmail] = useState("");
   const { login } = useContext(AuthContext);
   const [error, setError] = useState("");
+
+  const sanitizeInput = (input) => {
+    return DOMPurify.sanitize(input);
+  };
+
   const handleRegister = async (event) => {
     event.preventDefault();
+
+    const sanitizedUsername = sanitizeInput(username);
+    const sanitizedPassword = sanitizeInput(password);
+    const sanitizedEmail = sanitizeInput(email);
 
     // Regex patterns
     const usernamePattern = /^[a-zA-Z0-9_]{3,15}$/;
@@ -16,19 +26,19 @@ function RegistrationForm() {
     const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
     // Validation
-    if (!usernamePattern.test(username)) {
+    if (!usernamePattern.test(sanitizedUsername)) {
       setError(
         "Invalid username. It must be 3-15 characters long and can contain letters, numbers, and underscores."
       );
       return;
     }
-    if (!passwordPattern.test(password)) {
+    if (!passwordPattern.test(sanitizedPassword)) {
       setError(
         "Invalid password. It must be at least 8 characters long and contain at least one letter and one number."
       );
       return;
     }
-    if (!emailPattern.test(email)) {
+    if (!emailPattern.test(sanitizedEmail)) {
       setError("Invalid email format.");
       return;
     }
@@ -39,7 +49,11 @@ function RegistrationForm() {
         headers: {
           "Content-Type": "application/json",
         },
-        body: JSON.stringify({ username, password, email }),
+        body: JSON.stringify({
+          username: sanitizedUsername,
+          password: sanitizedPassword,
+          email: sanitizedEmail,
+        }),
         credentials: "include", // Necessary to ensure cookies are sent with the request
       });
 
@@ -50,7 +64,7 @@ function RegistrationForm() {
         setError(""); // Clear any previous error
       } else {
         const errorData = await response.json();
-        setError(errorData.error); // Set the error message from the response
+        setError(DOMPurify.sanitize(errorData.error)); // Set the error message from the response
       }
     } catch (error) {
       console.error("Error during registration:", error);
